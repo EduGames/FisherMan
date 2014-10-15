7f1f939ab81871308ce96a1324011570e6027d5d
Mohammed Header <mohheader@hotmail.com>
2014-10-15 14:01:38 +0200

    prepare for android.

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,7 +12,9 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/NumbersGame.cpp
+                   ../../Classes/NumbersGame.cpp \
+                   ../../Classes/ChicksGame.cpp \
+                   ../../Classes/Objects/Chick.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
